5c4f7e1307624ee0945cd1733fbb20887c01b5d0
olguk <olguk@DESKTOP-62BB9AA>
2021-01-17 22:31:21 +0300

    17 Ocak 2021

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void AddCustomer(Musteri musteri)
+        {
+            Console.WriteLine("\n");
+            Console.WriteLine("Müşteri adı:" + musteri.Name + " Müşteri soyadı:" + musteri.Surname
+                + " Müşteri id:" + musteri.Id + " \nMüşteri eklenmiştir.");
+        }
+
+        public void ListCustomer(Musteri musteri)
+        {
+
+            Console.WriteLine("---------");
+            Console.WriteLine(musteri.Id);
+                Console.WriteLine(musteri.Name);
+                Console.WriteLine(musteri.Surname);
+          
+           
+           
+        }
+
+        public void DeleteCustomer(Musteri musteri)
+        {
+            Console.WriteLine("\n");
+            Console.WriteLine("Müşteri adı:"+musteri.Name+ " Müşteri soyadı:"+musteri.Surname
+                +" Müşteri id:"+musteri.Id+ " \nMüşteri silinmiştir.");
+        }
+    }
+}
--- ClassMetotDemo/Program.cs
+++ ClassMetotDemo/Program.cs
@@ -6,7 +6,42 @@ namespace ClassMetotDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Musteri musteri1 = new Musteri();
+            musteri1.Name = "Burcu";
+            musteri1.Surname = "Eren";
+            musteri1.Id = 8970;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Name = "Elif";
+            musteri2.Surname = "Çetin";
+            musteri2.Id = 2674;
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Name = "Beliz";
+            musteri3.Surname = "Yılmaz";
+            musteri3.Id = 2546;
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3 };
+            MusteriManager musteriManager = new MusteriManager();
+
+            foreach (Musteri musteri in musteriler)
+            {
+                musteriManager.AddCustomer(musteri);
+              
+            }
+
+            Console.WriteLine("\nBütün müşteriler");
+            foreach (Musteri musteri in musteriler)
+            {
+                musteriManager.ListCustomer(musteri);
+            }
+
+            foreach (Musteri musteri in musteriler)
+            {
+                musteriManager.DeleteCustomer(musteri);
+            }
+
+
         }
     }
 }
